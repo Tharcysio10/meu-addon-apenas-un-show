5d3ea80d62684c56c123ce2961604acff9eea766
Tharcysio10 <120693856+Tharcysio10@users.noreply.github.com>
2026-05-24 16:38:17 +0200

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const addonInterface = require("./addon");
-
-serveHTTP(addonInterface, { port: 7000 });
-console.log("Addon rodando em http://localhost:7000/manifest.json");
